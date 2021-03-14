f291e5174269b8e4a17d1f65afc24f50d5376717
Kelsey Anderson <kjanderson@cs.uchicago.edu>
2021-03-14 07:25:25 -0500

    basic layout

--- src/app.js
+++ src/app.js
@@ -1,27 +1,155 @@
-// if the data you are going to import is small, then you can import it using es6 import
-// (I like to use use screaming snake case for imported json)
-// import MY_DATA from './app/data/example.json'
 
-import {myExampleUtil} from './utils';
-import {select} from 'd3-selection';
+import { convertNumbers, makeMenuData } from './utils';
+import { select } from 'd3-selection';
+import { sankey, sankeyLinkHorizontal, sankeyJustify } from 'd3-sankey';
+import { json, csv } from 'd3-fetch';
+
+
+// data sources
+
+const NODE_DATA = './data/plaNodes.json';
+const RATE_DATA = './data/plaNodes_demog.csv';
+
+
 // this command imports the css file, if you remove it your css wont be applied!
+
 import './main.css';
 
-// this is just one example of how to import data. there are lots of ways to do it!
-fetch('./data/example.json')
-  .then(response => response.json())
-  .then(data => myVis(data))
+
+// set the dimensions and margins of the graph
+
+var margin = { top: 10, right: 10, bottom: 50, left: 10 },
+  width = 650 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
+
+
+// import data for dynamic rates
+
+csv(RATE_DATA)
+  .then(data => {
+
+    let parsed = convertNumbers(data);
+    buildFilters(parsed);
+
+  })
   .catch(e => {
     console.log(e);
   });
 
-function myVis(data) {
-  const width = 5000;
-  const height = (36 / 24) * width;
-  console.log(data, height);
-  console.log('Hi!');
-  // EXAMPLE FIRST FUNCTION
-  select('#app')
-    .append('h1')
-    .text('hi!');
+
+// import data for basic sankey setup
+
+fetch(NODE_DATA)
+  .then(response => response.json())
+  .then(data => {
+
+    buildSankey(data);
+
+  })
+  .catch(e => {
+    console.log(e);
+});
+
+
+
+
+function buildFilters(data) {
+
+  var menus = makeMenuData(data);
+
+  var menuBar = select("#filters")
+    .append("div")
+    .style('display', 'flex')
+    .selectAll('.dropDown')
+    .data(d => Object.keys(menus))
+    .join('div');
+
+  menuBar.append('div').text(d => d);
+
+  menuBar
+    .append('select')
+    .join('select')
+    .attr('name', d => d)
+    .selectAll('option')
+    .data(d => menus[d])
+    .join('option')
+    .text(d => d)
+    .attr('value', d => d);
+
+  console.log(menus);
+  console.log(data);
+
 }
+
+
+function buildSankey(data) {
+
+  // append the svg object to the body of the page
+
+  var svg = select("#app").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+      "translate(" + margin.left + "," + margin.top + ")");
+
+
+  // Set the sankey diagram properties
+
+  let myVis = sankey()
+    .nodeWidth(150)
+    .nodePadding(2)
+    .size([width, height])
+    .nodeId(d => d.id)
+    .nodeAlign(sankeyJustify);
+
+  let graph = myVis(data);
+
+  // add in the links
+  let links = svg
+    .append("g")
+    .classed("links", true)
+    .selectAll("path")
+    .data(graph.links)
+    .enter()
+    .append("path")
+    .classed("link", true)
+    .attr("d", sankeyLinkHorizontal())
+    .attr("stroke-width", d => d.width)
+    .attr("id", d => d.source.class);
+
+  // add in the rectangles
+
+  let nodes = svg
+    .append("g")
+    .classed("nodes", true)
+    .selectAll("rect")
+    .data(graph.nodes)
+    .enter()
+    .append("rect")
+    .classed("node", true)
+    .attr("x", d => d.x0)
+    .attr("y", d => d.y0)
+    .attr("width", d => d.x1 - d.x0)
+    .attr("height", d => d.y1 - d.y0)
+    .attr("id", d => d.class);
+
+    // add in the title for the nodes
+
+  let labels = svg
+    .append("g")
+    .classed("labels", true)
+    .selectAll("text")
+    .data(graph.nodes)
+    .enter()
+    .append("text")
+    .attr("y", d => ( d.y0 + (d.y1 - d.y0 )/ 2))
+    .attr("x", d => ( d.x0 + (d.x1 - d.x0) / 2))
+    .attr("text-anchor", "middle")
+    .text(d => d.name);
+
+  // console.log(data);
+    
+}
+
+
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,51 @@
 // example of how to export functions
 // this particular util only doubles a value so it shouldn't be too useful
+
 export function myExampleUtil(x) {
   return x * 2;
+}
+
+// convert strings to numbers in CSV import
+
+export function convertNumbers(data) {
+  var arr = [];
+  for (var idx in data) {
+    var r = {};
+    var row = data[idx];
+
+    for (var [ k, val ] of Object.entries(row)) {
+      r[k] = Number(val);
+      if (isNaN(r[k])) {
+        r[k] = val;
+      }
+      }
+    arr.push(r);
+  }
+
+  return arr;
+}
+
+// create unique menus for text values in array
+
+export function makeMenuData(data) {
+  var menus = {};
+  for (var idx in data) {
+
+    var row = data[idx];
+
+    for (var [k, val] of Object.entries(row)) {
+
+      if (isNaN(val) && isNaN(k)) {
+
+        if (!(Object.keys(menus).includes(k))) {
+          menus[k] = [ val ];
+        }
+        else if (Array.isArray(menus[k]) &&
+          !(menus[k].includes(val))) {
+            menus[k].push(val);
+          }
+      }
+    }
+  }
+  return menus;
 }
