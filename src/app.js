aeea40364079f9a998d32a36b3d5256e4158102b
Kelsey Anderson <kjanderson@cs.uchicago.edu>
2021-03-15 05:42:59 -0500

    sankey data breaking

--- src/app.js
+++ src/app.js
@@ -1,15 +1,30 @@
 
-import { convertNumbers, makeMenuData } from './utils';
+import { prepData, updateData } from './utils';
 import { select } from 'd3-selection';
-import { sankey, sankeyLinkHorizontal, sankeyJustify } from 'd3-sankey';
+import {
+  sankey, sankeyLinkHorizontal, sankeyJustify
+} from 'd3-sankey';
 import { json, csv } from 'd3-fetch';
-
+import { transition } from 'd3-transition';
 
 // data sources
 
-const NODE_DATA = './data/plaNodes.json';
 const RATE_DATA = './data/plaNodes_demog.csv';
 
+// default starting selection
+
+let currentSelect = 'anaaaa';
+
+// menu indices for changing selection
+
+const MENU_ORDER = {
+  "school": 0,
+  "pla": 1,
+  "age": 2,
+  "race": 3,
+  "gender": 4,
+  "income": 5,
+};
 
 // this command imports the css file, if you remove it your css wont be applied!
 
@@ -18,18 +33,43 @@ import './main.css';
 
 // set the dimensions and margins of the graph
 
-var margin = { top: 10, right: 10, bottom: 50, left: 10 },
+const margin = { top: 10, right: 10, bottom: 50, left: 10 },
   width = 650 - margin.left - margin.right,
   height = 500 - margin.top - margin.bottom;
 
 
-// import data for dynamic rates
+// append the svg object to the body of the page
+
+let svg = select("#app").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform",
+    "translate(" + margin.left + "," + margin.top + ")");
+
+
+// Set the sankey diagram properties
+
+svg.append("g")
+  .classed("links", true);
+
+svg.append("g")
+  .classed("nodes", true);
+
+svg.append("g")
+  .classed("labels", true);
+
+// import data for sankey rates
 
 csv(RATE_DATA)
   .then(data => {
 
-    let parsed = convertNumbers(data);
-    buildFilters(parsed);
+    let filterData = buildFilters(data);
+    let selectedData = updateData(filterData, currentSelect);
+
+    // add sankey nodes and links
+
+    updateSankey(selectedData);
 
   })
   .catch(e => {
@@ -37,27 +77,14 @@ csv(RATE_DATA)
   });
 
 
-// import data for basic sankey setup
-
-fetch(NODE_DATA)
-  .then(response => response.json())
-  .then(data => {
-
-    buildSankey(data);
-
-  })
-  .catch(e => {
-    console.log(e);
-});
-
-
-
+// function for creating the dropdown menus
+// and swapping out the underlying data
 
 function buildFilters(data) {
 
-  var menus = makeMenuData(data);
+  const [menus, filters] = prepData(data);
 
-  var menuBar = select("#filters")
+  let menuBar = select("#filters")
     .append("div")
     .style('display', 'flex')
     .selectAll('.dropDown')
@@ -70,86 +97,134 @@ function buildFilters(data) {
     .append('select')
     .join('select')
     .attr('name', d => d)
+    .on("change", (event, row) => {
+
+      let menuIndex = MENU_ORDER[row];
+
+      let newSelect = currentSelect.slice(0, menuIndex) +
+        event.target.value.slice(0, 1) +
+        currentSelect.slice(menuIndex + 1, 6);
+
+      currentSelect = newSelect;
+
+      let newData = updateData(filters, newSelect);
+      updateSankey(newData);
+
+      // test code commented out
+      console.log(newData);
+      //console.log(newSelect);
+      console.log(event.target.value, row);
+
+    })
     .selectAll('option')
     .data(d => menus[d])
     .join('option')
     .text(d => d)
     .attr('value', d => d);
 
-  console.log(menus);
-  console.log(data);
+  // test code commented out
+  // console.log(menus);
+  // console.log(filters);
+
+  return filters;
 
 }
 
+// function for loading the nodes and lines 
 
-function buildSankey(data) {
+function updateSankey(data) {
 
-  // append the svg object to the body of the page
-
-  var svg = select("#app").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-      "translate(" + margin.left + "," + margin.top + ")");
-
-
-  // Set the sankey diagram properties
-
-  let myVis = sankey()
+  let graph = sankey()
     .nodeWidth(150)
     .nodePadding(2)
     .size([width, height])
     .nodeId(d => d.id)
     .nodeAlign(sankeyJustify);
 
-  let graph = myVis(data);
+  graph = graph(data);
 
-  // add in the links
+  var t = transition()
+    .duration(750);
+
+  // add in the links 
   let links = svg
-    .append("g")
-    .classed("links", true)
+    .selectAll("g .links")
+    .join("g .links");
+
+  let link = links
     .selectAll("path")
-    .data(graph.links)
-    .enter()
+    .data(graph.links);
+
+  link.enter()
     .append("path")
     .classed("link", true)
     .attr("d", sankeyLinkHorizontal())
     .attr("stroke-width", d => d.width)
     .attr("id", d => d.source.class);
 
-  // add in the rectangles
+    link
+      .transition(t)
+      .attr("d", sankeyLinkHorizontal())
+      .attr("stroke-width", d => d.width);
+
+  link.exit().remove();
 
   let nodes = svg
-    .append("g")
-    .classed("nodes", true)
+    .selectAll("g .nodes")
+    .join("g .nodes");
+
+  let node = nodes
     .selectAll("rect")
-    .data(graph.nodes)
-    .enter()
+    .data(graph.nodes,
+      d => d.id)
+    .classed("node", true);
+
+  if (!data.nodes[0].y0) {
+    console.log("Null Found!", graph);
+    return [];
+
+  }
+
+
+  // add in the rectangles
+
+  node.enter()
     .append("rect")
-    .classed("node", true)
     .attr("x", d => d.x0)
     .attr("y", d => d.y0)
     .attr("width", d => d.x1 - d.x0)
     .attr("height", d => d.y1 - d.y0)
     .attr("id", d => d.class);
 
-    // add in the title for the nodes
+  node.transition(t)
+    .attr("x", d => d.x0)
+    .attr("y", d => d.y0)
+    .attr("width", d => d.x1 - d.x0)
+    .attr("height", d => d.y1 - d.y0);
+
+  node.exit().remove();
+
+
+  // add in the title for the nodes
 
   let labels = svg
-    .append("g")
-    .classed("labels", true)
+    .select("g .labels")
+    .join("g .labels")
     .selectAll("text")
     .data(graph.nodes)
-    .enter()
-    .append("text")
-    .attr("y", d => ( d.y0 + (d.y1 - d.y0 )/ 2))
-    .attr("x", d => ( d.x0 + (d.x1 - d.x0) / 2))
+    .join("text")
+    .attr("y", d => (d.y0 + (d.y1 - d.y0) / 2))
+    .attr("x", d => (d.x0 + (d.x1 - d.x0) / 2))
     .attr("text-anchor", "middle")
-    .text(d => d.name);
+    .text(function (d) {
+      if ((d.y1 - d.y0) < 20) {
+        return '';
+      } else {
+        return d.name;
+      }
+    });
 
-  // console.log(data);
-    
+
 }
 
 
--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,6 @@
-// example of how to export functions
-// this particular util only doubles a value so it shouldn't be too useful
+// export functions
 
-export function myExampleUtil(x) {
-  return x * 2;
-}
-
-// convert strings to numbers in CSV import
+// convert strings to numbers in csv import
 
 export function convertNumbers(data) {
   var arr = [];
@@ -25,27 +20,191 @@ export function convertNumbers(data) {
   return arr;
 }
 
-// create unique menus for text values in array
 
-export function makeMenuData(data) {
-  var menus = {};
+// create unique menu listings and filterable json links
+
+export function prepData(data) {
+  let menus = {};
+  let filters = {};
+  
   for (var idx in data) {
 
     var row = data[idx];
+    let filterIndex = '';
 
     for (var [k, val] of Object.entries(row)) {
 
       if (isNaN(val) && isNaN(k)) {
 
+        filterIndex = filterIndex + val[0]
+
         if (!(Object.keys(menus).includes(k))) {
-          menus[k] = [ val ];
+          menus[k] = [val];
         }
         else if (Array.isArray(menus[k]) &&
           !(menus[k].includes(val))) {
-            menus[k].push(val);
-          }
+          menus[k].push(val);
+        }
       }
     }
+
+    let linkData = {
+
+        "links": [
+          {
+            "source": 0, "target": 4,
+            "value": Number(row['for-profit to for-profit'])
+          },
+          {
+            "source": 0, "target": 5,
+            "value": Number(row['for-profit to private non-profit'])
+          },
+          {
+            "source": 0, "target": 6,
+            "value": Number(row['for-profit to 2-year public'])
+          },
+          {
+            "source": 0, "target": 7,
+            "value": Number(row['for-profit to 4-year public'])
+          },
+          {
+            "source": 0, "target": 8,
+            "value": Number(row['for-profit to non-transfer'])
+          },
+          {
+            "source": 1, "target": 4,
+            "value": Number(row['private non-profit to for-profit'])
+          },
+          {
+            "source": 1, "target": 5,
+            "value": Number(row['private non-profit to private non-profit'])
+          },
+          {
+            "source": 1, "target": 6,
+            "value": Number(row['private non-profit to 2-year public'])
+          },
+          {
+            "source": 1, "target": 7,
+            "value": Number(row['private non-profit to 4-year public'])
+          },
+          {
+            "source": 1, "target": 8,
+            "value": Number(row['private non-profit to non-transfer'])
+          },
+          {
+            "source": 2, "target": 4,
+            "value": Number(row['2-year public to for-profit'])
+          },
+          {
+            "source": 2, "target": 5,
+            "value": Number(row['2-year public to private non-profit'])
+          },
+          {
+            "source": 2, "target": 6,
+            "value": Number(row['2-year public to 2-year public'])
+          },
+          {
+            "source": 2, "target": 7,
+            "value": Number(row['2-year public to 4-year public'])
+          },
+          {
+            "source": 2, "target": 8,
+            "value": Number(row['2-year public to non-transfer'])
+          },
+          {
+            "source": 3, "target": 4,
+            "value": Number(row['4-year public to for-profit'])
+          },
+          {
+            "source": 3, "target": 5,
+            "value": Number(row['4-year public to private non-profit'])
+          },
+          {
+            "source": 3, "target": 6,
+            "value": Number(row['4-year public to 2-year public'])
+          },
+          {
+            "source": 3, "target": 7,
+            "value": Number(row['4-year public to 4-year public'])
+          },
+          {
+            "source": 3, "target": 8,
+            "value": Number(row['4-year public to non-transfer'])
+          },
+          {
+            "source": 4, "target": 9,
+            "value": Number(row['for-profit to non-completers'])
+          },
+          {
+            "source": 5, "target": 9,
+            "value": Number(row['private non-profit to non-completers'])
+          },
+          {
+            "source": 6, "target": 9,
+            "value": Number(row['2-year public to non-completers'])
+          },
+          {
+            "source": 7, "target": 9,
+            "value": Number(row['4-year public to non-completers'])
+          },
+          {
+            "source": 8, "target": 9,
+            "value": Number(row['non-transfer to non-completers'])
+          },
+          {
+            "source": 4, "target": 10,
+            "value": Number(row['for-profit to graduates'])
+          },
+          {
+            "source": 5, "target": 10,
+            "value": Number(row['private non-profit to graduates'])
+          },
+          {
+            "source": 6, "target": 10,
+            "value": Number(row['2-year public to graduates'])
+          },
+          {
+            "source": 7, "target": 10,
+            "value": Number(row['4-year public to graduates'])
+          },
+          {
+            "source": 8, "target": 10,
+            "value": Number(row['non-transfer to graduates'])
+          }],
+
+        "cost": Number(row['cost']),
+        "time": Number(row['time'])
+      };
+
+      filters[filterIndex] = linkData;
+
   }
-  return menus;
+  return [ menus, filters ];
+}
+
+
+// splice node and filtered link value data together
+
+export function updateData(data, selection) {
+
+  let updated = {
+    "nodes": [
+      { "id": 0, "name": "for-profit", "class": "fp" },
+      { "id": 1, "name": "private non-profit", "class": "priv" },
+      { "id": 2, "name": "2-year public", "class": "pub2" },
+      { "id": 3, "name": "4-year public", "class": "pub4" },
+      { "id": 4, "name": "for-profit", "class": "fp" },
+      { "id": 5, "name": "private non-profit", "class": "priv" },
+      { "id": 6, "name": "2-year public", "class": "pub2" },
+      { "id": 7, "name": "4-year public", "class": "pub4" },
+      { "id": 8, "name": "non-transfer", "class": "nontran" },
+      { "id": 9, "name": "non-completers", "class": "nongrad" },
+      { "id": 10, "name": "graduates", "class": "grad" }],
+  }
+
+  updated['links'] = data[selection]['links'];
+  updated['time'] = data[selection]['time'];
+  updated['cost'] = data[selection]['cost'];
+
+  return updated;
 }
